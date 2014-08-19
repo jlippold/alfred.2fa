ba93cab6d5065b9bb1e66a9a40dbf412e042a40f
jlippold <jlippold@datacation.com>
2014-08-18 21:37:11 -0400

    fixes icons

--- src/lookup.js
+++ src/lookup.js
@@ -10,6 +10,11 @@ if (process.argv.length > 1) {
 	query = process.argv[2].toLowerCase();
 }
 
+var icon = {
+	type: "key",
+	value: "E71BC0F3-3905-4C17-AD6E-BE87BD48238B.png"
+};
+
 servers.forEach(function(server) {
 	if (!query || server.name.toLowerCase().substring(0, query.length) === query) {
 		items.addItem({
@@ -18,10 +23,11 @@ servers.forEach(function(server) {
 			title: server.name,
 			subtitle: 'Sends ' + server.name + ' one time password to clipboard',
 			valid: 'yes',
-			autocomplete: server.name
+			autocomplete: server.name,
+			icon: icon
 		});
 	}
-	id++; 
+	id++;
 });
 
 console.log(items.toString());
